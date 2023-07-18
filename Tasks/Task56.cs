b926da6ba7ac0117d9ca1a2920ceb1021c56c003
Dimishack <dimishack.git@mail.ru>
2023-07-18 19:46:17 +0300

    Выполнение задания №56

--- Tasks/Task56.cs
+++ Tasks/Task56.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HomeworkSeminar17072023.Tasks
 {
@@ -20,7 +17,47 @@ namespace HomeworkSeminar17072023.Tasks
     {
         internal static void MinSumOfRow()
         {
+            var random = new Random();
+            string rowsMin = String.Empty;
+            var array = new int[random.Next(4, 9), random.Next(4, 9)];
+            var sums = new int[array.GetLength(0)];
 
+            Console.WriteLine("Задача 56: Задайте прямоугольный двумерный массив. " +
+                "Напишите программу, которая будет находить строку с наименьшей суммой элементов." +
+                "\n" + new string('-', 30));
+            Console.WriteLine("Двумерный массив целых чисел (от 1 по 20)");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(1, 21);
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                int sum = 0;
+
+                Console.WriteLine("Сумма чисел {0} строки", i + 1);
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    sum += array[i, j];
+                    Console.Write(array[i, j]);
+                    if (j < array.GetLength(1) - 1)
+                        Console.Write(" + ");
+                }
+                sums[i] = sum;
+                Console.WriteLine(" = " + sum);
+                Console.WriteLine(new string('_', 20));
+            }
+            for (int i = 0; i < sums.Length;)
+            {
+                i = Array.IndexOf(sums, sums.Min(), i);
+                if (i == -1) break;
+                else rowsMin += ++i + " ";
+            }
+            Console.WriteLine("Номера строк с наименьшей суммой чисел: " + rowsMin);
         }
     }
 }
