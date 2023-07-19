72abc2b7dddcb42016cd0ee8ada4c8d75f350fe4
Dimishack <dimishack.git@mail.ru>
2023-07-19 17:28:18 +0300

    Первый рабочий код (работает не только с массивами 4х4)

--- Tasks/Task62.cs
+++ Tasks/Task62.cs
@@ -19,7 +19,57 @@ namespace HomeworkSeminar17072023.Tasks
     {
         internal static void FillSpiral()
         {
-
+            int number = 1;
+            int rowIndex = 0;
+            int columnIndex = 0;
+            int maxRow = 4;
+            int maxColumn = 4;
+            int minColumn = 0;
+            int minRow = 0;
+            var spiralArray = new int[maxRow, maxColumn];
+            do
+            {
+                while (columnIndex < maxColumn)
+                {
+                    spiralArray[rowIndex, columnIndex] = number++;
+                    columnIndex++;
+                }
+                rowIndex++;
+                columnIndex--;
+                while (rowIndex < maxRow)
+                {
+                    spiralArray[rowIndex, columnIndex] = number++;
+                    rowIndex++;
+                }
+                rowIndex--;
+                columnIndex--;
+                while (columnIndex >= minColumn)
+                {
+                    spiralArray[rowIndex, columnIndex] = number++;
+                    columnIndex--;
+                }
+                minRow++;
+                rowIndex--;
+                columnIndex++;
+                while (rowIndex >= minRow)
+                {
+                    spiralArray[rowIndex, columnIndex] = number++;
+                    rowIndex--;
+                }
+                minColumn++;
+                maxColumn--;
+                maxRow--;
+                rowIndex++;
+                columnIndex++;
+            } while (number <= spiralArray.Length);
+            for (int i = 0; i < spiralArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < spiralArray.GetLength(1); j++)
+                {
+                    Console.Write(spiralArray[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
