0b604c6aad639dee9b05575988e1566484a64702
Dimishack <dimishack.git@mail.ru>
2023-07-20 12:25:24 +0300

    Меню, встречающее пользователя

--- Program.cs
+++ Program.cs
@@ -9,20 +9,49 @@ namespace HomeworkSeminar17072023
         {
             try
             {
-                Console.WriteLine("Задача №54");
-                Task54.SortInDescending();
-                WaitUser();
-                Console.WriteLine("Задача №56");
-                Task56.MinSumOfRow();
-                WaitUser();
-                Console.WriteLine("Задача №58");
-                Task58.MultMatrices();
-                WaitUser();
-                Console.WriteLine("Задача №60");
-                Task60.ThreeDimensionalArray();
-                WaitUser();
-                Console.WriteLine("Доп: Задача №62");
-                Task62.FillSpiral();
+                ConsoleKey key;
+                Console.WriteLine("Добро пожаловать в программу с выполненными задачами №54, 56, 58, 60 и 62");
+                do
+                {
+                    Console.WriteLine("Выберите номер из списка с задачами, к которому Вы хотите перейти " +
+                        "\n(для выхода из программы нажмите <Enter>)");
+                    Console.WriteLine("1) Задача №54 \n2) Задача №56 \n3) Задача №58 \n4) Задача №60 \n5) Задача №62");
+                    key = Console.ReadKey(false).Key;
+                    Console.WriteLine();
+                    switch (key)
+                    {
+                        case ConsoleKey.Enter:
+                            break;
+                        case ConsoleKey.NumPad1:
+                        case ConsoleKey.D1:
+                            Task54.SortInDescending();
+                            WaitUser();
+                            break;
+                        case ConsoleKey.NumPad2:
+                        case ConsoleKey.D2:
+                            Task56.MinSumOfRow();
+                            WaitUser();
+                            break;
+                        case ConsoleKey.NumPad3:
+                        case ConsoleKey.D3:
+                            Task58.MultMatrices();
+                            WaitUser();
+                            break;
+                        case ConsoleKey.NumPad4:
+                        case ConsoleKey.D4:
+                            Task60.ThreeDimensionalArray();
+                            WaitUser();
+                            break;
+                        case ConsoleKey.NumPad5:
+                        case ConsoleKey.D5:
+                            Task62.FillSpiral();
+                            WaitUser();
+                            break;
+                        default:
+                            Console.WriteLine("Ошибка: Данного номера с задачей нет! Выберите другой.");
+                            break;
+                    }
+                } while (key != ConsoleKey.Enter);
             }
             catch (Exception ex)
             {
